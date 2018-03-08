5b433e79f0ea776334dc02ecbf3aacb3ebf8219d
Patrick Read <pread13@gmail.com>
2018-03-08 09:08:56 -0500

    storing the alarms in a JSON file

--- GetAlarms.py
+++ GetAlarms.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+# Retrieves alarms from Firebase DB when called, and stores in a local file at data/alarms.json
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import os
+import json
 
 cred = credentials.Certificate("credentials/firebase.json")
 default_app = firebase_admin.initialize_app(cred, {
   'databaseURL': os.environ['TM_FIREBASE_DB_URL']
 })
 
+# Read alarms from Firebase DB
 ref = db.reference('/alarms')
 user_id = os.environ['TM_USER_ID']
 alarms = list(ref.get()[user_id].values())
-print(alarms)
+
+# Write out to file
+file_handler = open("data/alarms.json", "w")
+file_handler.write(json.dumps(alarms))
+file_handler.close()
